5ec0eb0689053305f7d60a1cff9d4060d6eee3c0
SyhmiS <syahmidevz@gmail.com>
2024-12-21 02:58:01 +0800

    Initial to do app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <div>
+      <input 
+        type="text" 
+        v-model="newTask" 
+        placeholder="Enter a new task" 
+        @keyup.enter="addTask"
+      />
+      <button @click="addTask">Add</button>
+    </div>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        <span :class="{ completed: task.completed }" @click="toggleComplete(index)">
+          {{ task.text }}
+        </span>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() !== "") {
+        this.tasks.push({ text: this.newTask, completed: false });
+        this.newTask = "";
+      }
+    },
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    toggleComplete(index) {
+      this.tasks[index].completed = !this.tasks[index].completed;
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
+}
+.completed {
+  text-decoration: line-through;
+  color: gray;
+}
+button {
+  margin-left: 10px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+input {
+  padding: 5px;
+  width: 200px;
 }
 </style>
